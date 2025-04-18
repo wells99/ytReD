622727d8b1fa5ce825a88982fa760e42822dc339
well <9.9wellington9.9@gmail.com>
2025-04-18 15:36:55 -0300

    Navbar básico pronto

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Nav from "./components/Nav"
+import SideBar from "./components/sideBar"
+
+
 function App() {
   return (
     <>
       
       <div className="bg-neutral-800 h-dvh text-gray-100">
         <Nav></Nav>
+        <SideBar></SideBar>
         <h1 className="text-3xl pl-10 pt-10 font-bold">Funcionando</h1>
       </div>
     </>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,19 +1,26 @@
-import logo from "../assets/ytredLogo2.png"
+import logo from "../assets/ytredLogo.png"
 const Nav = () => {
     return ( 
         <>
             <div className="flex flex-row items-center justify-around pt-5 ">
-                <div>
-                    <img src={logo} alt="Youtube" className="w-40"/>
+                <div className="flex flex-row items-center justify-center gap-4">
+                    <span className="material-symbols-outlined hover:cursor-pointer text-white ">menu</span>
+                    <img src={logo} alt="Youtube" className="w-35"/>
                 </div>
-                <div className="w-[600px] text-center">
-                    <input type="text" placeholder="Pesquisar" className="w-[600px] rounded-md border-2 border-neutral-600" />
+                <div className="w-[600px] h-10 text-center flex flex-row bg-[#2A2A2A] border rounded-full items-center justify-around gap-2 px-4">
+                    <span className="material-symbols-outlined hover:cursor-pointer text-white">search</span>
+                    <input 
+                    type="text" 
+                    placeholder="Pesquisar" 
+                    maxLength={250}
+                    className="w-[600px] h-full rounded-full border-none focus:outline-none text-md" />
+                    <span className="material-symbols-outlined hover:cursor-pointer text-white">mic</span>
                 </div>
                 <div className="flex flex-row gap-8 pr-5">
-                    <button>+</button>
-                    <div>n</div>
-                    <div>conf</div>
-                    <div>perfil</div>
+                    <button><span className="material-symbols-outlined hover:cursor-pointer text-white ">add_circle</span></button>
+                    <button><span className="material-symbols-outlined hover:cursor-pointer text-white ">notifications</span></button>
+                    <button><span className="material-symbols-outlined hover:cursor-pointer text-white ">settings</span></button>
+                    <button><span className="material-symbols-outlined hover:cursor-pointer text-white ">account_circle</span></button>
                 </div>
             </div>
         </>
